7bbc1abdcbf9b02f7a53eb193adb1720409a0899
Nikita Kniazev <nok.raven@gmail.com>
2021-04-23 16:46:16 +0300

    pch test msvc automatic pch source generation

--- test/pch.py
+++ test/pch.py
@@ -15,10 +15,12 @@ t = BoostBuild.Tester()
 t.write("jamroot.jam", """
 import pch ;
 project : requirements <warnings-as-errors>on ;
-cpp-pch pch : pch.hpp : <toolset>msvc:<source>pch.cpp ;
-cpp-pch pch-afx : pch.hpp : <define>HELLO <toolset>msvc:<source>pch.cpp ;
+cpp-pch pch : pch.hpp ;
+cpp-pch pch-afx : pch.hpp : <define>HELLO ;
+cpp-pch pch-msvc-source : pch.hpp : <toolset>msvc:<source>pch.cpp ;
 exe hello : hello.cpp pch : <include>. ;
 exe hello-afx : hello-afx.cpp pch-afx : <define>HELLO <include>. ;
+exe hello-msvc-source : hello-msvc-source.cpp pch-msvc-source : <include>. ;
 """)
 
 pch_content = """\
@@ -40,7 +42,7 @@ if not toolset.startswith('clang') or toolset.startswith('clang-win'):
     include = '#include <pch.hpp>'
 else:
     include = ''
-for name in ("hello.cpp", "hello-afx.cpp"):
+for name in ("hello.cpp", "hello-afx.cpp", "hello-msvc-source.cpp"):
     t.write(name, include + """
 int main() { TestClass c(1, 2); }
 """)
@@ -48,6 +50,7 @@ int main() { TestClass c(1, 2); }
 t.run_build_system()
 t.expect_addition("bin/$toolset/debug*/hello.exe")
 t.expect_addition("bin/$toolset/debug*/hello-afx.exe")
+t.expect_addition("bin/$toolset/debug*/hello-msvc-source.exe")
 
 
 # Now make the header unusable, replace its content with some garbage, but
@@ -62,9 +65,11 @@ t.copy_timestamp("pch.hpp.orig", "pch.hpp")
 
 t.rm("bin/$toolset/debug*/hello.obj")
 t.rm("bin/$toolset/debug*/hello-afx.obj")
+t.rm("bin/$toolset/debug*/hello-msvc-source.obj")
 
 t.run_build_system()
 t.expect_addition("bin/$toolset/debug*/hello.obj")
 t.expect_addition("bin/$toolset/debug*/hello-afx.obj")
+t.expect_addition("bin/$toolset/debug*/hello-msvc-source.obj")
 
 t.cleanup()
