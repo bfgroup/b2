80b09ea0acf4c14a2c94cc2ef849f33db55b2634
René Ferdinand Rivera Morell <grafikrobot@gmail.com>
2021-09-08 22:04:47 -0500

    Back-copy changes from #72

--- src/engine/check_cxx11.cpp
+++ src/engine/check_cxx11.cpp
@@ -18,12 +18,14 @@ compiler to build the engine with.
 int main()
 {
     // Check for basic thread calls.
-    // [2020-08-19]
-    //   Mingw-w64 with win32 threading model (as opposed to posix threading model) does not really have std::thread etc.
-    //   Please see comments in sysinfo.cpp.
+    // [2020-08-19] Mingw-w64 with win32 threading model (as opposed to posix
+    // threading model) does not really have std::thread etc. Please see comments
+    // in sysinfo.cpp.
     #ifndef _WIN32
     { auto _ = std::thread::hardware_concurrency(); }
     #endif
 
-    const std::unique_ptr <float> pf {new float {3.14159f}};
+    // [2021-08-07] We check the following C++11 features: brace initialization,
+    // unique_ptr. Plus the author's ability to memorize some digits.
+    { const std::unique_ptr <float> pf {new float {3.14159f}}; }
 }
