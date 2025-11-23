a43f55306e48a15d4c99116ffc627f6f27242c89
René Ferdinand Rivera Morell <grafikrobot@gmail.com>
2025-11-23 11:51:22 -0600

    Fix author attribution/copyright.

--- test/escaping_dollar_before_round_bracket.py
+++ test/escaping_dollar_before_round_bracket.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright Vladimir Prus 2005.
+# Copyright Ivan Kotov 2025.
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at
 # https://www.bfgroup.xyz/b2/LICENSE.txt)
