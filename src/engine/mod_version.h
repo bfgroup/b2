e5173c53628b1855c9b09e811a990cf6be183f1c
Paolo Pastori <75467826+paolopas@users.noreply.github.com>
2026-03-21 14:38:20 +0100

    mod_version headers cleanup

--- src/engine/mod_version.h
+++ src/engine/mod_version.h
@@ -7,8 +7,10 @@ Distributed under the Boost Software License, Version 1.0.
 #ifndef B2_MOD_VERSION_H
 #define B2_MOD_VERSION_H
 
-#include "bind.h"
 #include "config.h"
+
+#include "bind.h"
+
 #include <vector>
 
 namespace b2 {
