f0a79ea2540a59175aabe4e1721857a86361ef34
Paolo Pastori <75467826+paolopas@users.noreply.github.com>
2026-03-29 14:58:10 +0200

    go for testing of builtin_glob_archive, everywhere

--- test/test_all.py
+++ test/test_all.py
@@ -326,14 +326,11 @@ if xml:
     critical_tests.insert(0, "collect_debug_info")
 
 tests = [
+    "builtin_glob_archive",
 ]
 
 exclusive_tests = [
 ]
-
-# Enable on macOS (Xcode) for troubleshooting
-if sys.platform == "darwin":
-    tests.append("builtin_glob_archive")
 
 if "--extras" in sys.argv:
     tests.append("boostbook")
