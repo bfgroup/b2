d05190c3be2d3a280d0ce185f986783a6edfff96
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-08 13:17:25 +0200

    CHORE: src: Adding .ci rules for ICX and example Jamfile for it
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ example/icx/hello.cpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2003 Vladimir Prus
+//  Copyright (c) 2026 Amlal El Mahrouss
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE.txt or copy at
+//  https://www.bfgroup.xyz/b2/LICENSE.txt)
+
+// tag::source[]
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello ICX!\n";
+}
+// end::source[]
